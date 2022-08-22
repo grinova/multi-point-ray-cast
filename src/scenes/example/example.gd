54d453593c7de5c031b3e56a5ed85b3c85323149
Aleksandr Kuzmin <grinova@gmail.com>
2022-08-22 04:46:12 +0300

    Added rotation ray begin position

--- src/scenes/example/example.gd
+++ src/scenes/example/example.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+const AUTO_RAY_POSITION := Vector2.ZERO
+const AUTO_RAY_CENTER := Vector2.ZERO
 const AUTO_RAY_RADIUS := 50.0
 const AUTO_RAY_ROTATION_STEP := 0.2 * PI
 
@@ -16,6 +18,8 @@ func _ready() -> void:
 func _physics_process(delta: float) -> void:
 	if autorotation:
 		_auto_ray_rotation += AUTO_RAY_ROTATION_STEP * delta
+		_ray.position = AUTO_RAY_POSITION
+		_ray.points[0] = AUTO_RAY_CENTER
 		_ray.points[1] = Vector2(AUTO_RAY_RADIUS, 0.0).rotated(_auto_ray_rotation)
 		_cast()
 
